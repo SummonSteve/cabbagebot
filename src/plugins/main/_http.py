e8828c43746b9a5de85b504448ce8bc4f86efc5a
Summon_Steve <30825190+SummonSteve@users.noreply.github.com>
2021-03-23 01:49:22 +0800

    fixed apis

--- src/plugins/main/_http.py
+++ src/plugins/main/_http.py
@@ -8,7 +8,7 @@ base_url = 'https://api-takumi.mihoyo.com'
 
 async def get_account_info(account_id: str):
     url = base_url + f'/game_record/card/wapi/getGameRecordCard?uid={account_id}'
-    header = get_header(account_id, False)
+    header = get_header(account_id, False, "2.3.0")
     async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=header) as resp:
                 return json.loads(await resp.text())
@@ -17,7 +17,7 @@ async def get_account_info(account_id: str):
 async def get_ys_info(server: str, role_id: str):
     account_id = None
     url = base_url + f'/game_record/genshin/api/index?server={server}&role_id={role_id}'
-    header = get_header(account_id, False)
+    header = get_header(account_id, False, "2.3.0")
     async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=header) as resp:
                 return json.loads(await resp.text())
@@ -26,7 +26,7 @@ async def get_ys_info(server: str, role_id: str):
 async def get_abyss_info(server: str, role_id: str, schedule_type: int):
     account_id = None
     url = base_url + f'/game_record/genshin/api/spiralAbyss?server={server}&role_id={role_id}&schedule_type={schedule_type}'
-    header = get_header(account_id, False)
+    header = get_header(account_id, False, "2.3.0")
     async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=header) as resp:
                 return json.loads(await resp.text())
@@ -34,7 +34,7 @@ async def get_abyss_info(server: str, role_id: str, schedule_type: int):
 
 async def post_bbsReward(account_id: str, game_role_id: str, region: str, cookie_token: str):
     url = base_url + '/event/bbs_sign_reward/sign'
-    header = get_header(account_id, cookie_token)
+    header = get_header(account_id, cookie_token, "2.3.0")
     postContent = '{{"act_id": "e202009291139501","region": "{}","uid": "{}"}}'.format(region, game_role_id)
     data = json.loads(postContent)
     async with aiohttp.ClientSession()as session:
@@ -44,7 +44,7 @@ async def post_bbsReward(account_id: str, game_role_id: str, region: str, cookie
 
 async def get_ys_finance_info(account_id, game_role_id, region, cookie_token):
     url = f'https://hk4e-api.mihoyo.com/event/ys_ledger/monthInfo?month=0&bind_uid={game_role_id}&bind_region={region}&bbs_presentation_style=fullscreen&bbs_auth_required=true&utm_source=icon&utm_medium=ys&utm_campaign=GameRecord'
-    header = get_header(account_id, cookie_token)
+    header = get_header(account_id, cookie_token, "2.3.0")
     async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=header) as resp:
                 return json.loads(await resp.text())
--- src/plugins/main/mhy_http_header.py
+++ src/plugins/main/mhy_http_header.py
@@ -5,11 +5,8 @@ import string
 import uuid
 
 
-cookie_token = 'your_token'
-account_id = 'you_account_id'
-mhyVersion = '2.3.0'
-bot_cookie = f'account_id={account_id};cookie_token={cookie_token}'
-
+cookie_token = 'XA6FfYpPwcyDhj6gpjToicMXeDPYEOPkl6D6wbsT'
+bot_cookie = f'account_id=73247611;cookie_token={cookie_token}'
 
 def md5(text):
     md5 = hashlib.md5()
@@ -17,18 +14,24 @@ def md5(text):
     return md5.hexdigest()
 
 
-def get_ds():
-    n = "h8w582wxwgqvahcdkpvdhbh2w9casgfl"
+def get_ds(mhyVersion):
+    if (mhyVersion == "2.1.0"):
+        n = md5(mhyVersion)
+    elif (mhyVersion == "2.3.0"):
+        n = "h8w582wxwgqvahcdkpvdhbh2w9casgfl"
+    else:
+        mhyVersion = "2.4.0"
+        n = "pbcfcvnfsm5s2w4x3lsq8caor7v8nlqm"
     i = str(int(time.time()))
     r = ''.join(random.sample(string.ascii_lowercase + string.digits, 6))
     c = md5(f"salt={n}&t={i}&r={r}")
     return f"{i},{r},{c}"
 
 
-def get_header(account_id, user_cookie_token):
+def get_header(account_id, user_cookie_token, mhyVersion):
     header = {
         'Accept': 'application/json, text/plain, */*',
-        'DS': get_ds(),
+        'DS': get_ds(mhyVersion),
         'Origin': 'https://webstatic.mihoyo.com',
         'x-rpc-app_version': mhyVersion,
         'User-Agent': f'Mozilla/5.0 (Linux; Android 9; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Mobile Safari/537.36 miHoYoBBS/{mhyVersion}',
